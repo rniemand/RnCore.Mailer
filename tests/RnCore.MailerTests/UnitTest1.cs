0aec205a23ca06f0f2efb77c21ee178b5355b6b6
Richard Niemand <niemand.richard@gmail.com>
2023-01-05 19:23:02 -0700

    Stubbing test project

---
+++ tests/RnCore.MailerTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RnCore.MailerTests
+{
+  public class Tests
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      Assert.Pass();
+    }
+  }
+}
